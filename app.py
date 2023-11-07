fd1de092bbc5aac8c9d0061c1b8049ac34c678f9
Suryakanta-Karan <suryakantakaran93@gmail.com>
2023-11-07 21:52:39 +0530

    Quiz 3.

--- app.py
+++ app.py
@@ -1,60 +1,53 @@
 import os
-import cv2
+from flask import Flask, request, jsonify
+from PIL import Image
 import numpy as np
-from flask import Flask, request, render_template, jsonify
+from skimage import measure
 
 app = Flask(__name__)
 
-# Load the pre-trained digit recognition model (Random Forest, for example)
-from sklearn.datasets import fetch_openml
-from sklearn.ensemble import RandomForestClassifier
+# Helper function to compute structural similarity index between two images
+def compare_images(image1, image2):
+    # Convert the images to grayscale
+    image1 = image1.convert('L')
+    image2 = image2.convert('L')
 
-mnist = fetch_openml("mnist_784")
-X, y = mnist.data, mnist.target
-model = RandomForestClassifier(n_estimators=100)
-model.fit(X, y)
+    # Convert images to numpy arrays
+    image1_array = np.array(image1)
+    image2_array = np.array(image2)
 
-# Create a function to process and predict digits
-def process_and_predict_digit(image):
+    # Compute the Structural Similarity Index (SSIM)
+    ssim = measure.compare_ssim(image1_array, image2_array)
+
+    return ssim
+
+# API route to check if two images are similar
+@app.route('/check_similarity', methods=['POST'])
+def check_similarity():
     try:
-        # Load and preprocess the image
-        img = cv2.imdecode(np.fromstring(image.read(), np.uint8), cv2.IMREAD_COLOR)
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        img = cv2.resize(img, (28, 28))
-        img = img.reshape(1, -1)
+        # Check if two image files are provided in the request
+        if 'image1' not in request.files or 'image2' not in request.files:
+            return jsonify({"error": "Two image files are required."}), 400
 
-        # Predict the digit
-        predicted_digit = model.predict(img)[0]
-
-        return predicted_digit
-
-    except Exception as e:
-        print(str(e))
-        return -1
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/compare_digits', methods=['POST'])
-def compare_digits():
-    try:
-        # Receive two images
         image1 = request.files['image1']
         image2 = request.files['image2']
 
-        if image1 and image2:
-            digit1 = process_and_predict_digit(image1)
-            digit2 = process_and_predict_digit(image2)
+        if image1.filename == '' or image2.filename == '':
+            return jsonify({"error": "Image filenames are empty."}), 400
 
-            if digit1 == digit2:
-                return jsonify({'result': True})
-            else:
-                return jsonify({'result': False})
+        image1 = Image.open(image1)
+        image2 = Image.open(image2)
+
+        # Compare the images for similarity
+        similarity = compare_images(image1, image2)
+
+        if similarity > 0.7:  # Adjust the threshold as needed
+            return jsonify({"result": True, "similarity": similarity})
+        else:
+            return jsonify({"result": False, "similarity": similarity})
 
     except Exception as e:
-        print(str(e))
-        return jsonify({'result': False})
+        return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)

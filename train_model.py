990b23c5b8aa0235a9dc8d242f16f72dcb5b92e3
Suryakanta-Karan <suryakantakaran93@gmail.com>
2023-12-02 17:38:42 +0530

    final_exam_Q1.

--- train_model.py
+++ train_model.py
@@ -1,13 +1,7 @@
+from sklearn import datasets, preprocessing
 import itertools
-from sklearn import datasets, svm
 import joblib
 from utils import hyperparameter_tuning, prepare_data_splits
-
-import os
-
-#os.makedirs('/home/suryakantak/hand-written/app/model/')
-#print(directory)
-
 
 def main():
     # Load the digits dataset
@@ -18,6 +12,9 @@ def main():
     flattened_data = digits_data.images.reshape((n_samples, -1))
     X_data = flattened_data
     y_data = digits_data.target
+
+    # Normalize the data
+    X_data_normalized = preprocessing.normalize(X_data, norm='l2')
 
     # Define parameter ranges
     gamma_values = [0.001, 0.01, 0.1, 1, 100]
@@ -30,8 +27,8 @@ def main():
     size_combinations = list(itertools.product(test_sizes, dev_sizes))
 
     for test_frac, dev_frac in size_combinations:
-        # Split the data into train, dev, and test sets
-        X_train, Y_train, X_dev, Y_dev, X_test, Y_test = prepare_data_splits(X_data, y_data, test_frac, dev_frac)
+        # Split the normalized data into train, dev, and test sets
+        X_train, Y_train, X_dev, Y_dev, X_test, Y_test = prepare_data_splits(X_data_normalized, y_data, test_frac, dev_frac)
 
         # Tune hyperparameters
         trained_model, _, _, _, _ = hyperparameter_tuning(X_train, Y_train, X_dev, Y_dev, all_param_combinations)
--- utils.py
+++ utils.py
@@ -1,6 +1,9 @@
 import itertools
 from sklearn.model_selection import train_test_split
 from sklearn import svm, metrics
+from sklearn import svm, datasets
+from sklearn.model_selection import train_test_split, GridSearchCV
+
 
 def prepare_data_splits(data, labels, test_fraction, dev_fraction):
     X_train_dev, X_test, Y_train_dev, Y_test = train_test_split(
@@ -36,3 +39,59 @@ def hyperparameter_tuning(X_train, Y_train, X_dev, Y_dev, param_combinations):
             best_C = C
 
     return best_model, best_gamma, best_C, best_accuracy, best_train_accuracy
+
+# Read digits
+def read_digits():
+    digits = datasets.load_digits()
+    x = digits.images
+    y = digits.target
+    return x, y
+
+# We will define utils here:
+def preprocess_data(data):
+    n_samples = len(data)
+    data = data.reshape((n_samples, -1))
+    return data
+
+# Split data into training, development, and test subsets
+def split_train_dev_test(X, y, test_size, dev_size, random_state=1):
+    # First, split data into training and temporary test subsets
+    X_train_dev, X_test, y_train_dev, y_test = train_test_split(
+        X, y, test_size=test_size, random_state=random_state
+    )
+    
+    # Next, split the remaining data (X_train_dev, y_train_dev) into training and development subsets
+    X_train, X_dev, y_train, y_dev = train_test_split(
+        X_train_dev, y_train_dev, test_size=dev_size, random_state=random_state
+    )
+    
+    return X_train, X_dev, X_test, y_train, y_dev, y_test
+
+# Create a classifier: a support vector classifier
+def train_model(X, y, model_params, model_type='svm'):
+    if model_type == 'svm':
+        clf = svm.SVC(**model_params)
+    clf.fit(X, y)
+    return clf
+
+# Function to predict and evaluate the model
+def predict_and_eval(model, X_test, y_test):
+    # Predict the value of the digit on the test subset
+    predicted = model.predict(X_test)
+
+    # Quantitative sanity check
+    return predicted
+
+# Function for hyperparameter tuning
+def tune_hparams(X, y, X_dev, y_dev, param_grid, model_type='svm'):
+    if model_type == 'svm':
+        clf = svm.SVC()
+    
+    grid_search = GridSearchCV(clf, param_grid, cv=3)
+    grid_search.fit(X, y)
+    
+    best_hparams = grid_search.best_params_
+    best_model = grid_search.best_estimator_
+    best_accuracy = grid_search.best_score_
+    
+    return best_hparams, best_model, best_accuracy

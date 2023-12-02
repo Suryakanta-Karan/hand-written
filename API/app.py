d5b36777bf1ac26b74da6ebf7dec581313c822cf
Suryakanta-Karan <suryakantakaran93@gmail.com>
2023-12-02 19:27:23 +0530

    Final Exam Commit.

--- API/app.py
+++ API/app.py
@@ -6,7 +6,6 @@ from io import BytesIO
 import os
 
 # Load the model
-#model = joblib.load('/digits/API/best_model.pkl')
 model = joblib.load('best_model.pkl')
 SAVED_MODEL_DIR = "../saved_model"
 app = Flask(__name__)
@@ -17,32 +16,13 @@ normalizer = Normalizer(norm='l2')
 
 models = {}
 
-
-def load_model():
+def load_models():
     models['svm'] = joblib.load(os.path.join(SAVED_MODEL_DIR, 'svm_model.joblib'))
     models['lr'] = joblib.load(os.path.join(SAVED_MODEL_DIR, 'lr_model.joblib'))
     models['tree'] = joblib.load(os.path.join(SAVED_MODEL_DIR, 'tree_model.joblib'))
 
 # Load models
-load_model()
-
-# @app.route('/predict_digit', methods=['POST'])
-# def predict_digit():
-#     if 'image' not in request.files:
-#         return jsonify(error='Please provide an image.'), 400
-
-#     image_bytes = request.files['image'].read()
-#     image = Image.open(BytesIO(image_bytes)).convert('L')
-#     image = image.resize((8, 8), Image.LANCZOS)
-    
-#     # image_arr = np.array(image).reshape(1, -1)
-#     # pred = model.predict(image_arr)
-    
-#     image_arr = np.array(image).reshape(1, -1)
-    # image_arr_normalized = normalizer.transform(image_arr)
-    # pred = model.predict(image_arr_normalized)
-
-#     return jsonify(predicted_digit=int(pred[0]))
+load_models()
 
 @app.route('/predict/<model_type>', methods=['POST'])
 def predict_digit(model_type):
@@ -64,7 +44,6 @@ def predict_digit(model_type):
 
     return jsonify(predicted_digit=int(pred[0]))
 
-
 @app.route('/compare_digits', methods=['POST'])
 def compare_digits():
     if 'image1' not in request.files or 'image2' not in request.files:
@@ -79,18 +58,12 @@ def compare_digits():
     image1 = image1.resize((8, 8), Image.LANCZOS)
     image2 = image2.resize((8, 8), Image.LANCZOS)
     
-    # image1_arr = np.array(image1).reshape(1, -1)
-    # image2_arr = np.array(image2).reshape(1, -1)
-
-    # pred1 = model.predict(image1_arr)
-    # pred2 = model.predict(image2_arr)
-    
     image1_arr = np.array(image1).reshape(1, -1)
     image2_arr = np.array(image2).reshape(1, -1)
 
     image1_arr_normalized = normalizer.transform(image1_arr)
     image2_arr_normalized = normalizer.transform(image2_arr)
-    print(f"[DEBUG]: Normalized the image")
+    
     pred1 = model.predict(image1_arr_normalized)
     pred2 = model.predict(image2_arr_normalized)
 
